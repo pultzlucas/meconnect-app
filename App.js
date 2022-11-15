f74ecdb1e15e60f6bcc7285112faf700c04da789
pultzlucas <pultzlucas@gmail.com>
2022-11-15 11:53:17 -0300

    hide api url

--- App.js
+++ App.js
@@ -38,10 +38,10 @@ Notifications.setNotificationHandler({
 });
 
 export default function App() {
-  useEffect(() => {
+  // useEffect(() => {
     // Api.config.setApiDomain('http://44.202.106.85/api')
-    Api.config.setApiDomain('http://192.168.15.177:80/api')
-  }, [])
+    // Api.config.setApiDomain('http://192.168.15.177:80/api')
+  // }, [])
 
   const notificationListener = useRef();
   const responseListener = useRef();
